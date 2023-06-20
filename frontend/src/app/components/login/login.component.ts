04b229b320ba265ffc83be409477ca8c2166f718
kove.vit@gmail.com <kove.vit@gmail.com>
2023-06-21 00:26:23 +0300

    front init

---
+++ frontend/src/app/components/login/login.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { AuthenticationService } from 'src/app/services/authentication.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],
+})
+export class LoginComponent {
+  constructor(private authService: AuthenticationService) {}
+
+  login() {
+    this.authService.login('', '').subscribe((data) => console.log('success'));
+  }
+}
